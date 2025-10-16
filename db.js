89312d26b7a772564a9b6864ce1873e21667d64d
alessandravg1122@gmail.com <alessandravg2228@gmail.com>
2025-10-16 14:40:15 -0400

    Backend: Modificar el db para desplegar.

--- db.js
+++ db.js
@@ -1,16 +1,27 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+
 const DB_PORT = parseInt(process.env.DB_PUERTO, 10) || 5432;
+const isProduction = process.env.NODE_ENV === 'production' || process.env.RENDER === 'true'; 
 
 const sequelize = new Sequelize(
-    process.env.DB_NOMBRE,
-    process.env.DB_USUARIO,
-    process.env.DB_CLAVE,
-    {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'postgres',
-        port: DB_PORT,
-        logging: false
-    }
+    process.env.DB_NOMBRE,
+    process.env.DB_USUARIO,
+    process.env.DB_CLAVE,
+    {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'postgres',
+        port: DB_PORT,
+        logging: false,
+
+        dialectOptions: isProduction ? {
+            ssl: {
+                require: true, 
+                rejectUnauthorized: false 
+            }
+        } : {}
+     
+    }
 );
+
 module.exports = sequelize;
